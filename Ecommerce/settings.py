534fcc11a8a1f85f2d91ffd8ae2667ac46b6f0f8
Trần Huỳnh Kỳ Anh <56480941+tranhuynhkyanh@users.noreply.github.com>
2023-10-08 16:04:19 +0700

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-g=jzhv5zv8nxar2z0jpv#jv-$o*p(+e597*wgkau$gc&7v*m4e')
-DEBUG = False'
+DEBUG = False
 ADMIN_ENABLED = False
 ALLOWED_HOSTS = ['*']
 
