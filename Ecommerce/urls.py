1e25e189bf811cdb3fcd8dd0de3bb3c7c8f52b5b
Trần Huỳnh Kỳ Anh <56480941+tranhuynhkyanh@users.noreply.github.com>
2023-10-08 15:49:44 +0700

    Update urls.py

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("",include("core.urls")),
     path("accounts/",include("userauth.urls")),
     path('accounts/', include('allauth.urls'))
@@ -26,3 +27,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.ADMIN_ENABLED is True:
+    urlpatterns += [path('admin/', admin.site.urls),]
