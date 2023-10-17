6a7e6659b807b6ae241fc65c5fef69747b6d8b26
Trần Huỳnh Kỳ Anh <56480941+tranhuynhkyanh@users.noreply.github.com>
2023-10-17 19:59:35 +0700

    Update asgi.py

--- Ecommerce/asgi.py
+++ Ecommerce/asgi.py
@@ -8,13 +8,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
+from wsgi import *
 from django.urls import path
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.consumers import *
 from channels.auth import AuthMiddlewareStack
-from wsgi import *
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Ecommerce.settings")
 
 application = get_asgi_application()
