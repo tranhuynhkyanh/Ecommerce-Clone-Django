6d27703e8da5900b5215952e764ea7b4a82733d2
Trần Huỳnh Kỳ Anh <56480941+tranhuynhkyanh@users.noreply.github.com>
2023-10-17 19:27:07 +0700

    update

--- userauth/models.py
+++ userauth/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.import datetime
 from django.core.cache import cache
-from django.conf import settings
 import datetime
 
 def user_directory_path(instance,filename):
@@ -16,20 +15,7 @@ class User(AbstractUser):
 
     def __str__(self) :
         return self.email
-    def last_seen(self):
-        return cache.get('seen_%s' % self.username)
 
-    def online(self):
-        if self.last_seen():
-            now = datetime.datetime.now()
-            if now > self.last_seen() + datetime.timedelta(
-                         seconds=settings.USER_ONLINE_TIMEOUT):
-                return False
-            else:
-                return True
-        else:
-            return False
-        
 class Follow(models.Model):
     follower = models.ForeignKey(User,on_delete=models.CASCADE,related_name="follower")
     following = models.ForeignKey(User,on_delete=models.CASCADE,related_name="following")
