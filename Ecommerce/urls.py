3d47a4082c96ec49bf3a898d85ee4b273f249c19
Trần Huỳnh Kỳ Anh <56480941+tranhuynhkyanh@users.noreply.github.com>
2023-11-01 21:02:04 +0700

    Update urls.py

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-
+from django.urls import re_path
 urlpatterns = [
     path("",include("core.urls")),
     path("accounts/",include("userauth.urls")),
