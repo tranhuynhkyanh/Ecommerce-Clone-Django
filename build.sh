0a1bce7c990ad95196336deeb5eac8cf625d9889
Trần Huỳnh Kỳ Anh <56480941+tranhuynhkyanh@users.noreply.github.com>
2023-11-01 20:01:15 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate --run-syncdb
 python -m celery -A Ecommerce worker -l info --pool=solo
+python -m gunicorn myproject.asgi:application
