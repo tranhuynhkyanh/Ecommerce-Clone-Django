8ce737031f686ddf3577f1ad53ca44c50c95db90
Trần Huỳnh Kỳ Anh <56480941+tranhuynhkyanh@users.noreply.github.com>
2023-11-01 19:17:06 +0700

    fix celery

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic 
+python manage.py makemigrations
 python manage.py migrate --run-syncdb
+celery -A Ecommerce worker -l info --pool=solo
